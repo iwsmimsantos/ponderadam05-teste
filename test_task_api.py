569935046c46e3626b107c3d4383435137fd2dd9
iwsmimsantos <iasmim.jesus@sou.inteli.edu.br>
2025-03-20 03:44:26 -0300

    feat: cria função que verifica instituições na API

--- test_task_api.py
+++ test_task_api.py
@@ -4,4 +4,9 @@ import requests
 api_url = 'https://two025-1a-t13-es05-api2.onrender.com/api/v1/institutions'
 
 # Token do grupo 3 para autenticação
-auth_token = 'Bearer g3-d72f4d0e90fce94208a9cc383c7e10ae7b
+auth_token = 'Bearer g3-d72f4d0e90fce94208a9cc383c7e10ae7b'
+
+def test_get_institutions():
+    headers = {
+        'Authorization': auth_token
+    }
